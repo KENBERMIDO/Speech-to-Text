597b7b9e9d1d6a0aaf76f86f0491eb0963c0c7b7
cppro <kenb@live.wsd1.org>
2023-06-07 00:14:12 -0500

    a

--- src/speech_to_text.py
+++ src/speech_to_text.py
@@ -2,6 +2,6 @@
 
 import whisper
 
-model = whisper.load_model("small.en")
-result = model.transcribe("sample-0.mp3", FP16=False)
+model = whisper.load_model("small")
+result = model.transcribe("output.wav")
 print(result["text"])
